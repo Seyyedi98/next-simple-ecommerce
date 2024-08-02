4c0b4fca5b1360aab9db32f54fcb67214447f4c6
Seyyedi <mr.seyyedi76@gmail.com>
2024-08-02 21:49:05 +0330

    Create navbar

---
+++ app/admin/layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Nav, NavLink } from "../_components/Nav";
+
+const AdminLayout = ({ children }) => {
+  return (
+    <>
+      <Nav>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/products">Products</NavLink>
+        <NavLink href="/admin/user">Customers</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
+      </Nav>
+      <div className="container my-6">{children}</div>
+    </>
+  );
+};
+
+export default AdminLayout;
